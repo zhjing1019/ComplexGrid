c237c930616c266f5ffbc0d48ca04ac32abd3f02
张静 <zhjing1019@gmail.com>
2019-11-04 16:11:31 +0800

    complexGrid

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    // 'standard'
   ],
   // required to lint *.vue files
   plugins: [
